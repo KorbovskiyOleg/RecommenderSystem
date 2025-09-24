66e02e02e973c8cd6cca880fda0aeb33e96ba019
Car_Bit <olegkorbovskij@gmail.com>
2025-09-24 20:16:14 +0300

    Проверка интеграции Weka

--- RecommenderSystem/src/main/java/MLJavaBook/RecommenderSystem/App.java
+++ RecommenderSystem/src/main/java/MLJavaBook/RecommenderSystem/App.java
@@ -5,6 +5,10 @@ import org.apache.mahout.math.Vector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class App {
     
     private static final Logger logger = LoggerFactory.getLogger(App.class);
@@ -17,7 +21,8 @@ public class App {
             // Создаем простой вектор
             Vector v1 = new DenseVector(new double[]{1.0, 2.0, 3.0});
             Vector v2 = new DenseVector(new double[]{4.0, 5.0, 6.0});
-
+            
+            
             
             // Вычисляем скалярное произведение
             double dotProduct = v1.dot(v2);
@@ -34,9 +39,30 @@ public class App {
                 logger.info("Расчет выполнен успешно!");
             }
             
+         // Код Weka для работы с данными
+            logger.info("Загрузка данных Weka...");
             
+            // Загрузка данных - убедитесь, что файл data.arff находится в правильной папке
+            DataSource source = new DataSource("src/main/java/MLJavaBook/RecommenderSystem/data.arff");
+            Instances data = source.getDataSet();
+            
+            // Установка класса (последний атрибут)
+            if (data.classIndex() == -1) {
+                data.setClassIndex(data.numAttributes() - 1);
+            }
+            
+            // Создание и обучение классификатора
+            J48 tree = new J48();
+            tree.buildClassifier(data);
+            
+            logger.info("Weka успешно подключена!");
+            logger.info("Количество экземпляров: {}", data.numInstances());
+            logger.info("Количество атрибутов: {}", data.numAttributes());
+            logger.info("Дерево решений:\n{}", tree.toString());
             
             logger.info("Приложение завершено.");
+            
+           
             
         } catch (Exception e) {
             logger.error("Критическая ошибка: ", e);
