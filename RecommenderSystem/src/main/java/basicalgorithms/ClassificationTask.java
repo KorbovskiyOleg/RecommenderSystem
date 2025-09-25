5b00f0c00c90802f5a10410d7f255cc35ec5f2a4
Car_Bit <olegkorbovskij@gmail.com>
2025-09-25 20:46:34 +0300

    удаляем атрибут названия animal

--- RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
+++ RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
@@ -2,15 +2,27 @@ package basicalgorithms;
 
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
 
 public class ClassificationTask {
     
     public static void main(String[] args) throws Exception { 
         
+    	//Загружаем данные
         DataSource source = new DataSource("data/zoo.arff");
         Instances data = source.getDataSet();
         
         System.out.println(data.numInstances() + " instance loaded");
+        
+     // Удаляем первsq атрибут - animal
+     		String[] opts = new String[] { "-R", "1" };
+     		Remove remove = new Remove();
+     		remove.setOptions(opts);
+     		remove.setInputFormat(data);
+     		data = Filter.useFilter(data, remove);
+     		
+     		
     }
 }
