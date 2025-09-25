68d1253809524a167757d58e4fabac55b8129be8
Car_Bit <olegkorbovskij@gmail.com>
2025-09-26 01:41:42 +0300

    Подробное описание загрузки и фильтрации данных

--- RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
+++ RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
@@ -5,24 +5,31 @@ import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
 
-
 public class ClassificationTask {
-    
-    public static void main(String[] args) throws Exception { 
-        
-    	//Загружаем данные
-        DataSource source = new DataSource("data/zoo.arff");
-        Instances data = source.getDataSet();
-        
-        System.out.println(data.numInstances() + " instance loaded");
-        
-     // Удаляем первsq атрибут - animal
-     		String[] opts = new String[] { "-R", "1" };
-     		Remove remove = new Remove();
-     		remove.setOptions(opts);
-     		remove.setInputFormat(data);
-     		data = Filter.useFilter(data, remove);
-     		
-     		
-    }
+
+	public static void main(String[] args) throws Exception {
+
+		/*
+		 * Загружаем данные
+		 */
+
+		DataSource source = new DataSource("data/zoo.arff");
+		Instances data = source.getDataSet();
+
+		System.out.println(data.numInstances() + " instance loaded");
+		System.out.println(data.numAttributes() + " attributes");
+		System.out.println(data.toString());
+
+		// Удаления шумных, избыточных или ненужных атрибутов перед обучением модели!
+		
+		// Удаляем первый атрибут - animal
+		String[] opts = new String[] { "-R", "1" };// Настройка параметров: удалить атрибуты 1 и 3 (индексация с 1)
+		Remove remove = new Remove();// Создание фильтра удаления атрибутов
+		remove.setOptions(opts);
+		remove.setInputFormat(data);// Указание формата входных данных
+		data = Filter.useFilter(data, remove);// применяем фильтр к набору данных
+		
+		
+
+	}
 }
