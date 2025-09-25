ce6afc63160353dbc159f8ecbe8c670afb70cc55
Car_Bit <olegkorbovskij@gmail.com>
2025-09-25 19:24:30 +0300

    создал файл с данными. Провекра загрузки

--- RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
+++ RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
@@ -1,9 +1,17 @@
 package basicalgorithms;
 
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class ClassificationTask {
 	
-	public static void main(String[] args) { 
+	public static void main(String[] args) throws Exception{ 
 		
-	System.out.println("hello");
+		/*
+		 * Load the data
+		 */
+		DataSource source = new DataSource("data/zoo.arff");
+		Instances data = source.getDataSet();
+		System.out.println(data.numInstances() + " instances loaded.");
 	}
 }
