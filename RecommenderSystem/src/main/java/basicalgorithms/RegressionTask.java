3a1a6cf32e5907bd00525d37561fd3292a4f5af0
Car_Bit <olegkorbovskij@gmail.com>
2025-09-26 08:08:47 +0300

    Создал новый файл с классом main алгоритма регрессии, залил данные

---
+++ RecommenderSystem/src/main/java/basicalgorithms/RegressionTask.java
@@ -0,0 +1,23 @@
+package basicalgorithms;
+
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import java.io.File;
+
+public class RegressionTask {
+	
+	public static void main(String[] args) throws Exception {
+		
+		
+		/*
+		 * Load data
+		 */
+		CSVLoader loader = new CSVLoader();
+		loader.setFieldSeparator(",");
+		loader.setSource(new File("data/ENB2012_data.csv"));
+		Instances data = loader.getDataSet();
+
+		 //System.out.println(data);
+	}
+
+}
