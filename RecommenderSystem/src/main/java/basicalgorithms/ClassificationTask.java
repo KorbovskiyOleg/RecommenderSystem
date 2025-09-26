b56a544d0fe0dc880356806a66ed1034a6be87f5
Car_Bit <olegkorbovskij@gmail.com>
2025-09-26 05:15:27 +0300

    Вывод классификации животного на обученных данных

--- RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
+++ RecommenderSystem/src/main/java/basicalgorithms/ClassificationTask.java
@@ -1,11 +1,14 @@
 package basicalgorithms;
 
+
 import javax.swing.JFrame;
 
 import weka.attributeSelection.AttributeSelection;
 import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.classifiers.trees.J48;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -68,7 +71,8 @@ public class ClassificationTask {
 		J48 tree = new J48();//Создаем объект дерева решений (аналог C4.5) - пустое дерево
 		tree.setOptions(options);//Передаем параметры настройки алгоритму J48(алгоритм теперь знает, что нужно строить дерево без pruning)
 		tree.buildClassifier(data);//Обучаем дерево решений на данных (data). Алгоритм анализирует данные,Строит дерево решений по алгоритму C4.5
-		System.out.println(tree);//выводим дерево 
+		System.out.println(tree.toString());//выводим дерево 
+		
 		
 		/*
 		 * Визуализация
@@ -82,25 +86,39 @@ public class ClassificationTask {
 		frame.getContentPane().add(tv);
 		frame.setVisible(true);
 		tv.fitToScreen();
+		
+		
+		/*
+		 * Классификация новых данных
+		 */
+		double[] vals = new double[data.numAttributes()];//Создаеv массив для хранения значений всех атрибутов
+		vals[0] = 0.0; // hair {false, true} - заполнение характеристик животного
+		vals[1] = 1.0; // feathers {false, true}
+		vals[2] = 1.0; // eggs {false, true}
+		vals[3] = 0.0; // milk {false, true}
+		vals[4] = 1.0; // airborne {false, true}
+		vals[5] = 0.0; // aquatic {false, true}
+		vals[6] = 0.0; // predator {false, true}
+		vals[7] = 0.0; // toothed {false, true}
+		vals[8] = 1.0; // backbone {false, true}
+		vals[9] = 1.0; // breathes {false, true}
+		vals[10] = 0.0; // venomous {false, true}
+		vals[11] = 0.0; // fins {false, true}
+		vals[12] = 2.0; // legs INTEGER [0,9]
+		vals[13] = 1.0; // tail {false, true}
+		vals[14] = 0.0; // domestic {false, true}
+		vals[15] = 0.0; // catsize {false, true}
+		
+		Instance myUnicorn = new DenseInstance(1.0, vals);//Создаем новый экземпляр животного с заданными характеристиками(вес экземпляра (обычно 1.0))
+		
+		myUnicorn.setDataset(data); // Связываем экземпляр с структурой dataset чтобы алгоритм понимал типы атрибутов (номинальные, числовые)
+
+		double label = tree.classifyInstance(myUnicorn);// Дерево решений классифицирует животное - числовой код класса (например, 0 для "mammal", 1 для "bird")
+		System.out.println(data.classAttribute().value((int) label));// Преобразуем числовой код в текстовое название класса
+
 
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
+
 }
